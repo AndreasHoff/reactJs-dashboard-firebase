b631ae0e453c35631c7c69aeeaa86313260b541e
Andreas <mchoffn@hotmail.com>
2023-12-07 17:02:48 +0100

    added left and right containers

---
+++ src/components/MainContainer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './MainContainer.css'
+
+function MainContainer() {
+  return (
+    <div className='main-container'>
+        <div className="left"></div>
+        <div className="right"></div>
+    </div>
+  )
+}
+
+export default MainContainer
