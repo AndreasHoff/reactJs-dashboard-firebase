5c6817742301991ab0dfb455f886a6d3fe77d549
Andreas <mchoffn@hotmail.com>
2023-12-30 21:20:14 +0100

    working on left banner section

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -4,7 +4,21 @@ import './MainContainer.css'
 function MainContainer() {
   return (
     <div className='main-container'>
-        <div className="left"></div>
+        <div className="left">
+            <div className="banner">
+                <div className="text-container">
+                    <h1>Round Hall</h1>
+                    <h2>1.5 ETH/h2</h2>
+                    <p>Uploaded by Alexander Vernoff</p>
+                    <div className="bid">
+                        <a href='/' className="button-one">
+                            Bid now
+                        </a>
+                        <p>Ending in<span>2d:15h:31m</span></p>
+                    </div>
+                </div>
+            </div>
+        </div>
         <div className="right"></div>
     </div>
   )
